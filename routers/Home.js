6b9a5d5017c650e68876130af3e329e00f403c9a
mohamed-1995-hassan <sunsherp@gmail.com>
2021-01-24 13:13:57 -0800

    Update Home.js
    uuu

--- routers/Home.js
+++ routers/Home.js
@@ -8,26 +8,25 @@ router.get('/', async (req, res, next) => {
 
     const Arr=[];
 
-    // try {
-    //     const user = await getAll();
-    //     for(let i=0;i<user.length;i++)
-    //     {
-    //         Arr.push(...user[i].Blogs.slice(-1));
-    //     }
+    try {
+        const user = await getAll();
+        for(let i=0;i<user.length;i++)
+        {
+            Arr.push(...user[i].Blogs.slice(-1));
+        }
        
-    //   const blogArr=[];
+      const blogArr=[];
 
-    //     for(let j=0;j<Arr.length;j++)
-    //     {
-    //         blogArr.push(await getbyId(Arr[j]));
-    //     }
+        for(let j=0;j<Arr.length;j++)
+        {
+            blogArr.push(await getbyId(Arr[j]));
+        }
 
-    //   res.json(blogArr);
+      res.json(blogArr);
 
-    // } catch (e) {
-    //     next(e);
-    // }
-    res.send("hello")
+    } catch (e) {
+        next(e);
+    }
 
 })
 
